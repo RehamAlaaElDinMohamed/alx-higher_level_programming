28cfde117929a70ce7a1825cefabcea8cfa71c73
rehamalaaeldinmohamed <rehamalaaeldinmohamed@gmail.com>
2023-07-11 06:36:21 -0700

    updated

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
-"""Defines a Python class-to-JSON function."""
+#!/usr/bin/python3
+"""This module defines a Python class-to-JSON function"""
 
 
 def class_to_json(obj):
-    """Return the dictionary represntation of a simple data structure."""
+    """Returns the dictionary representation of a simple data structure"""
     return obj.__dict__
