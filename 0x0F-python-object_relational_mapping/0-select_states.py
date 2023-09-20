098d96acc836c7c5003bda0a71b07838140876fd
Reham Alaa El-Din Mohamed <105158735+RehamAlaaElDinMohamed@users.noreply.github.com>
2023-09-20 19:35:43 +0300

    Updated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,37 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""Script to list all states from the database hbtn_0e_0_usa"""
+
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
+    # Get MySQL username, password, and database name from command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+
+    # Create a cursor object to execute SQL queries
+    cursor = db.cursor()
+
+    # Execute the SQL query to select all states and order them by states.id
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the rows from the query result
+    rows = cursor.fetchall()
+
+    # Display the results
     for row in rows:
         print(row)
-    cur.close()
+
+    # Close the cursor and database connection
+    cursor.close()
     db.close()
